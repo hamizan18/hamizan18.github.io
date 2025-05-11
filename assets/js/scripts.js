72c130158e962463c0fbe68ec57356af352cd674
Hamizan Desu <hamizanputra321@gmil.com>
2025-05-11 20:25:43 +0700

    nambah bg music

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,15 @@ function myFunction()
 {
     alert("Kan sudah bilang jangan di klik!");
 }
-function myFunction()
+function myfunction()
 {
     alert("Ya emang bole tpi gada efek sama aja hehe..")
 }
+
+const bgMusic = document.getElementById('bgMusic');
+
+document.addEventListener('click', () => {
+  bgMusic.play().catch(e => {
+    console.log('Autoplay blocked until user interaction', e);
+  });
+});
